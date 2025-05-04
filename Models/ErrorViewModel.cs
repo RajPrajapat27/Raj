9a7ce4c8960ea9712952e376ad87b6a18aab0ad0
RajPrajapat27 <rajprajapat2778@gmail.com>
2025-05-04 19:29:19 +0530

    Old data , no longer needed.

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace dateandtime.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
